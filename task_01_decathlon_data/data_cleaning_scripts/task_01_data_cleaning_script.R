174e2393ef6e7448e7c8f86b89b5ffb0e2d7b431
lloydlombardi <108779828+lloydlombardi@users.noreply.github.com>
2022-07-31 11:17:48 +0100

    Update column names

--- task_01_decathlon_data/data_cleaning_scripts/task_01_data_cleaning_script.R
+++ task_01_decathlon_data/data_cleaning_scripts/task_01_data_cleaning_script.R
@@ -3,18 +3,28 @@ library(tidyverse)
 library(janitor)
 library(readr)
 library(assertr)
+library(stringr)
 
 # Read in the raw data
 decathlon <- read_rds("raw_data/decathlon.rds")
 
-# View the raw data
-# view(decathlon)
-
-# Run `clean names` on the column names
-decathlon_clean <- clean_names(decathlon)
+# Change row_names to first column 'athlete_name'
+decathlon <- tibble::rownames_to_column(decathlon, var = "athlete_name")
 
 # View the data with cleaned names
 # view(decathlon_clean)
 
-# Change row_names to first column 'athlete_name'
-decathlon_clean <- tibble::rownames_to_column(decathlon_clean, var = "athlete_name")
+
+decathlon <- decathlon %>% 
+  pivot_wider(names_from = Competition,
+              values_from = Points)
+
+# Run `clean names` on the column names
+decathlon_clean <- clean_names(decathlon)
+
+# Remove
+decathlon_clean <- decathlon_clean %>% 
+  rename_with(~ gsub("^x", "", .x))
+
+view(decathlon_clean)
+
