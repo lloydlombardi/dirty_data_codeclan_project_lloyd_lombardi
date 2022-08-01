a266e4cffde427d9528903344e9948cf7f2aa5ff
lloydlombardi <108779828+lloydlombardi@users.noreply.github.com>
2022-08-01 22:45:14 +0100

    created data cleaning in RMD as R kept crashing

--- task_04_halloween_candy_data/data_cleaning_scripts/task_04_data_cleaning_script.R
+++ task_04_halloween_candy_data/data_cleaning_scripts/task_04_data_cleaning_script.R
@@ -10,6 +10,31 @@ candy_2015 <- read_excel("raw_data/boing-boing-candy-2015.xlsx")
 candy_2016 <- read_excel("raw_data/boing-boing-candy-2016.xlsx")
 candy_2017 <- read_excel("raw_data/boing-boing-candy-2017.xlsx")
 
-view(candy_2015)
-view(candy_2016)
-view(candy_2017)
+# Run `clean_names` function on datasets
+candy_2015 <- clean_names(candy_2015)
+candy_2016 <- clean_names(candy_2016)
+candy_2017 <- clean_names(candy_2017)
+
+candy_2015 %>%
+  names()
+
+# candy_2016 %>% 
+#   names()
+
+# candy_2017 %>%
+#   names()
+
+
+# candy_2016 %>% 
+#   distinct(which_country_do_you_live_in) %>% 
+#   pull()
+
+# view(candy_2015)
+# view(candy_2016)
+# view(candy_2017)
+
+
+candy_2015 <- candy_2015 %>% 
+  pivot_longer(c(4:96, 115),
+               names_to = "candies",
+               values_to = "rating")
