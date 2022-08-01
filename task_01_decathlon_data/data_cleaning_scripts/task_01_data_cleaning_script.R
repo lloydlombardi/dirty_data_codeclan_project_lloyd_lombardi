9bad0a7383de17e9a74ff5dd60ff75dfedf9bbca
lloydlombardi <108779828+lloydlombardi@users.noreply.github.com>
2022-08-01 09:41:06 +0100

    Create column names script and add units to column names

--- task_01_decathlon_data/data_cleaning_scripts/task_01_data_cleaning_script.R
+++ task_01_decathlon_data/data_cleaning_scripts/task_01_data_cleaning_script.R
@@ -11,9 +11,6 @@ decathlon <- read_rds("raw_data/decathlon.rds")
 # Change row_names to first column 'athlete_name'
 decathlon <- tibble::rownames_to_column(decathlon, var = "athlete_name")
 
-# View the data with cleaned names
-# view(decathlon_clean)
-
 # Change athletes' names to title
 decathlon <- decathlon %>% 
   mutate(athlete_name = str_to_title(athlete_name))
@@ -28,6 +25,17 @@ decathlon_clean <- decathlon_clean %>%
 # Relocate the rank, points, and competition columns to after athletes' names
 decathlon_clean <- decathlon_clean %>% 
   relocate(c(rank:competition), .after = athlete_name)
+
+# Source in column names
+source("data_cleaning_scripts/event_columns.R")
+
+# Add in seconds units to the race columns
+decathlon_clean <- decathlon_clean %>% 
+  rename_with(~str_replace(., "$", "_(s)"), .cols = starts_with(race_columns))
+
+# Add in meters units to the athletics event columns
+decathlon_clean <- decathlon_clean %>% 
+  rename_with(~str_replace(., "$", "_(m)"), .cols = starts_with(athletics_columns))
 
 # View dataset
 view(decathlon_clean)
