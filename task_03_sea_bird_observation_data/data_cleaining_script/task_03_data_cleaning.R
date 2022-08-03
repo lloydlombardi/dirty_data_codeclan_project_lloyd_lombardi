e39860d5cea273686bb0c5c98b4044d571c7b529
lloydlombardi <108779828+lloydlombardi@users.noreply.github.com>
2022-08-03 20:03:51 +0100

    Quick attempt at task 3

---
+++ task_03_sea_bird_observation_data/data_cleaining_script/task_03_data_cleaning.R
@@ -0,0 +1,41 @@
+# Load in libs
+library(readxl)
+library(tidyverse)
+library(stringr)
+library(janitor)
+
+# Read in excel sheets
+bird_data <- read_xls("raw_data/seabirds.xls", sheet = "Bird data by record ID")
+ship_data <- read_xls("raw_data/seabirds.xls", sheet = "Ship data by record ID")
+
+# Run clean names
+bird_data <- clean_names(bird_data)
+ship_data <- clean_names(ship_data)
+
+# Left join tables by ID
+seabird_data <- bird_data %>% 
+  left_join(ship_data, by = "record_id")
+
+# Select relevant columns
+seabird_data <- seabird_data %>% 
+  select(record_id,
+         species_common_name_taxon_age_sex_plumage_phase,
+         species_scientific_name_taxon_age_sex_plumage_phase,
+         species_abbreviation,
+         count,
+         lat)
+
+# Rename column
+seabird_data <- seabird_data %>% 
+  rename("common_name" = "species_common_name_taxon_age_sex_plumage_phase")
+
+# Rename column
+seabird_data <- seabird_data %>% 
+  rename("scientific_name" = "species_scientific_name_taxon_age_sex_plumage_phase")
+
+# Rename column
+seabird_data <- seabird_data %>% 
+  rename("abbreviation" = "species_abbreviation")
+
+#Write csv
+write_csv(seabird_data, "clean_data/seabird_data.csv")
